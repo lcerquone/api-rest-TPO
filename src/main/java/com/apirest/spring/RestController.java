bc1c178c7940e4cc21cff8f396dcc683cc2264cb
Luis <laclvl99@gmail.com>
2021-10-10 15:04:08 -0300

    Metodos nuevos RestController

--- src/main/java/com/apirest/spring/RestController.java
+++ src/main/java/com/apirest/spring/RestController.java
@@ -7,7 +7,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import vo.campeonatoVO;
 import vo.clubVO;
+import vo.faltaVO;
+import vo.golVO;
 import vo.jugadorVO;
+import vo.miembroVO;
+import vo.partidoVO;
 import vo.responsableVO;
 import controlador.ControladorTPO;
 import exceptions.usuarioException;
@@ -22,6 +26,37 @@ public class RestController {
 	public clubVO getClub(@RequestParam(name = "legajo") Integer legajo) throws usuarioException {
 		return ControladorTPO.getInstance().ObtenerClubPorIdVO(legajo);
 	} 
+	
+	@RequestMapping("/getJugadorById")
+	public jugadorVO getJugador(@RequestParam(name = "idJugador") Integer idJugador) throws usuarioException {
+		return ControladorTPO.getInstance().ObtenerJugadorPorIdVO(idJugador);
+	} 
+	
+	@RequestMapping("/getFaltaById")
+	public faltaVO getFalta(@RequestParam(name = "idFalta") Integer idFalta) throws usuarioException {
+		return ControladorTPO.getInstance().ObtenerFaltaPorIdVO(idFalta);
+	}
+	
+	@RequestMapping("/getGolById")
+	public golVO getGol(@RequestParam(name = "idGol") Integer idGol) throws usuarioException {
+		return ControladorTPO.getInstance().ObtenerGolPorIdVO(idGol);
+	}
+	
+	@RequestMapping("/getPartidoById")
+	public partidoVO getPartido(@RequestParam(name = "idPartido") Integer idPartido) throws usuarioException {
+		return ControladorTPO.getInstance().ObtenerPartidoPorIdVO(idPartido);
+	}
+	
+	@RequestMapping("/getResponsableById")
+	public responsableVO getResponsable(@RequestParam(name = "idResponsable") Integer idResponsable) throws usuarioException {
+		return ControladorTPO.getInstance().ObtenerResponsablePorIdVO(idResponsable);
+	}
+	
+	@RequestMapping("/getMiembroById")
+	public miembroVO getMiembro(@RequestParam(name = "idMiembro") Integer idMiembro) throws usuarioException {
+		return ControladorTPO.getInstance().ObtenerMiembroPorIdVO(idMiembro);
+	}
+	
 	@PostMapping("/addParticipante")
 	public void addParticipante(@RequestParam(name = "idClub")Integer idClub,@RequestParam(name = "idCampeonato")Integer idCampeonato) throws usuarioException {
 		ControladorTPO.getInstance().agregarParticipante(idClub, idCampeonato);
@@ -45,6 +80,26 @@ public class RestController {
 	@PostMapping("/addClub")
 	public void addJugador(@RequestBody clubVO club) throws usuarioException {
 		ControladorTPO.getInstance().agregarClubVO(club);
+	}
+	
+	@PostMapping("/addMiembro")
+	public void addMiembro(@RequestParam (name = "idJugador") Integer idJugador,@RequestParam (name = "idClub") Integer idClub,@RequestParam (name = "idPartido") Integer idPartido) throws usuarioException {
+		ControladorTPO.getInstance().agregarMiembroVO(idJugador,idPartido,idClub);
+	}
+	
+	@PostMapping("/addPartido")
+	public void addPartido(@RequestBody partidoVO club,@RequestParam (name= "idClubL")Integer idClubL,@RequestParam (name= "idClubV")Integer idClubV,@RequestParam (name= "idCampeonato")Integer idCampeonato) throws usuarioException {
+		ControladorTPO.getInstance().agregarPartidoVO(club,idClubL,idClubV,idCampeonato);
+	}
+	
+	@PostMapping("/addGol")
+	public void addGol(@RequestParam (name= "idJugador")Integer idJugador,@RequestParam (name= "idPartido")Integer idPartido,@RequestParam (name= "sentido")String sentido,@RequestParam (name= "minuto")Integer minuto) throws usuarioException {
+		ControladorTPO.getInstance().agregarGolVO(idJugador,idPartido,sentido,minuto);
+	}
+	
+	@PostMapping("/addFalta")
+	public void addFalta(@RequestParam (name= "idCampeonato")Integer idCampeonato,@RequestParam (name= "idPartido")Integer idPartido,@RequestParam (name= "idJugador")Integer idJugador,@RequestParam (name= "tipo")String tipo,@RequestParam (name= "minuto")Integer minuto) throws usuarioException {
+		ControladorTPO.getInstance().agregarFaltaVO(idCampeonato,idJugador,idPartido,tipo,minuto);
 	}
 }
 
